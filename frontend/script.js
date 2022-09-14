dbb2e3a6c89c3eab51cfff968d4eb9ccfe54fb57
L.Evi <92294344+evelntv@users.noreply.github.com>
2022-09-14 11:42:59 +0200

    Add variables

---
+++ frontend/script.js
@@ -0,0 +1,22 @@
+let MyVariable = "value";
+console.log(MyVariable);
+
+// Nem haszanlunk var-t//
+
+MyVariable = "new value";
+console.log(MyVariable);
+
+MyVariable = 1;
+console.log(MyVariable);
+
+MyVariable = true;
+console.log(MyVariable);
+
+let MyVariable2 = "text";
+console.log(MyVariable2);
+
+MyVariable = MyVariable2;
+console.log(MyVariable);
+
+let MyVariable3 = MyVariable + MyVariable2;
+console.log(MyVariable3);
