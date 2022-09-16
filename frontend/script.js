256815c250e60a9271d14a687f8f9999b9bd7c11
L.Evi <92294344+evelntv@users.noreply.github.com>
2022-09-16 09:17:58 +0200

    Add intial content

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,30 @@
 let MyVariable = "value";
-console.log(MyVariable);
+//console.log(MyVariable);
 
 // Nem haszanlunk var-t//
 
 MyVariable = "new value";
-console.log(MyVariable);
+//console.log(MyVariable);
 
 MyVariable = 1;
-console.log(MyVariable);
+//console.log(MyVariable);
 
 MyVariable = true;
-console.log(MyVariable);
+//console.log(MyVariable);
 
 let MyVariable2 = "text";
-console.log(MyVariable2);
+//console.log(MyVariable2);
 
 MyVariable = MyVariable2;
-console.log(MyVariable);
+//console.log(MyVariable);
 
 let MyVariable3 = MyVariable + MyVariable2;
-console.log(MyVariable3);
+//console.log(MyVariable3);
+
+function sumVariables() {
+  console.log(variable1 + variable2);
+}
+
+sumVariables(MyVariable, MyVariable2);
+sumVariables(8, nyolc);
+sumVariables("word1" + "word2");
